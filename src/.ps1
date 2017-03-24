f927bd34235217c89ee1c3b19d9081609d8c1b40
lespinal <luis.espinal@vertivco.com>
2017-03-24 21:01:45 +0100

    FRIST\!(10+1)

---
+++ src/.ps1
@@ -0,0 +1,11 @@
+ export TERM=xterm-256color
+
+. ${HOME}/.ctrl
+
+. ${HOME}/.vcsutils
+
+export OLD_PS1="\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$"
+
+export PS1="\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\e[35m\]\`git_prompt\`\e[0m\]\n\$"
+
+
